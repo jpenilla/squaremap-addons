afe2da5f28c184f63c7321b52af7eeffefe00c42
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-09-08 14:24:51 -0700

    worldguard: only add flags to list in v3 config if mode is blacklist

--- addons/worldguard/src/main/java/xyz/jpenilla/squaremap/addon/worldguard/config/WGConfig.java
+++ addons/worldguard/src/main/java/xyz/jpenilla/squaremap/addon/worldguard/config/WGConfig.java
@@ -3,10 +3,12 @@ package xyz.jpenilla.squaremap.addon.worldguard.config;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.plugin.Plugin;
+import org.spongepowered.configurate.ConfigurationNode;
 import org.spongepowered.configurate.NodePath;
 import org.spongepowered.configurate.transformation.ConfigurationTransformation;
 import org.spongepowered.configurate.transformation.TransformAction;
 import xyz.jpenilla.squaremap.addon.common.config.Config;
+import xyz.jpenilla.squaremap.addon.common.config.ListMode;
 
 public final class WGConfig extends Config<WGConfig, WGWorldConfig> {
     public WGConfig(Plugin plugin) {
@@ -26,17 +28,20 @@ public final class WGConfig extends Config<WGConfig, WGWorldConfig> {
             .build();
 
         final ConfigurationTransformation twoToThree = ConfigurationTransformation.builder()
-            .addAction(NodePath.path("world-settings", "default", "flags", "list"), (path, node) -> {
-                final List<String> list = new ArrayList<>(node.getList(String.class, List.of()));
-                list.addAll(List.of(
-                    "squaremap-stroke-color",
-                    "squaremap-stroke-weight",
-                    "squaremap-stroke-opacity",
-                    "squaremap-fill-color",
-                    "squaremap-fill-opacity",
-                    "squaremap-click-tooltip"
-                ));
-                node.setList(String.class, list);
+            .addAction(NodePath.path("world-settings", "default", "flags"), (path, node) -> {
+                if (node.node("list-mode").get(ListMode.class, ListMode.BLACKLIST) == ListMode.BLACKLIST) {
+                    final ConfigurationNode listNode = node.node("list");
+                    final List<String> list = new ArrayList<>(listNode.getList(String.class, List.of()));
+                    list.addAll(List.of(
+                        "squaremap-stroke-color",
+                        "squaremap-stroke-weight",
+                        "squaremap-stroke-opacity",
+                        "squaremap-fill-color",
+                        "squaremap-fill-opacity",
+                        "squaremap-click-tooltip"
+                    ));
+                    listNode.setList(String.class, list);
+                }
                 return null;
             })
             .build();
