4d07cd5e5353647ae92b488b2c3b72e9425db2cb
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-09-21 00:14:19 -0700

    Update run-paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     base
-    id("xyz.jpenilla.run-paper") version "2.1.0"
+    id("xyz.jpenilla.run-paper") version "2.2.0"
 }
 
 allprojects {
@@ -27,5 +27,10 @@ runPaper {
 tasks {
     runServer {
         minecraftVersion("1.20.1")
+        downloadPlugins {
+            hangar("squaremap", "1.2.0")
+            url("https://ci.enginehub.org/repository/download/bt10/22612:id/worldedit-bukkit-7.2.16-SNAPSHOT-dist.jar?guest=1")
+            url("https://ci.enginehub.org/repository/download/bt11/22585:id/worldguard-bukkit-7.0.10-SNAPSHOT-dist.jar?guest=1")
+        }
     }
 }
